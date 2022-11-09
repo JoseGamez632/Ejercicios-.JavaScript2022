a34eb8af98b73ecc2ff7d3e7235ba05020ebca88
JoseGamez632 <jlgamez0@misena.edu.co>
2022-11-08 22:14:14 -0500

    Create main.js

---
+++ Js/main.js
@@ -0,0 +1,14 @@
+// alert('Hola munndo, estoy aprendiendo JavaScrip');
+
+console.log('Hola este es un mensaje por consola')
+
+
+//TIPOS DE DATOS
+
+console.log(123456789)
+console.log('jOSE')
+console.log(false)
+
+alert()
+
+document.write('Hola, estoy escribiendo en la pagina web');
