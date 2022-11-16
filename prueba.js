7551ff975efe84be5ec4c4cbb34679ff5f8b14dd
JoseGamez632 <jlgamez0@misena.edu.co>
2022-11-15 21:05:44 -0500

    Create prueba.js

---
+++ prueba.js
@@ -0,0 +1,21 @@
+// esta es una prueba.
+
+var base=4;
+var altura=3;
+const NUM=2;
+
+var area;
+
+area = base * altura / NUM;
+alert (area);
+console.log(area);
+document.write (area);
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
